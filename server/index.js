587fa5be516fcfdbf3a0acbcb81cef67f1291f65
Akash Ahmed <akashahmedlikhon.007@gmail.com>
2024-09-26 21:54:03 +0600

    database and rooms collection create

--- server/index.js
+++ server/index.js
@@ -48,6 +48,12 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // auth related api
+    const roomCollection = client.db('Click-N-Book').collection('rooms')
+    app.get('/rooms', async(req,res)=>{
+      const result = await roomCollection.find().toArray()
+      res.send(result)
+    })
+
     app.post('/jwt', async (req, res) => {
       const user = req.body
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
